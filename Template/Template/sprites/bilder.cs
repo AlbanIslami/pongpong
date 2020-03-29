d2de838d41915e3921f350aac9d696065ee9103c
alban <alban2001@hotmail.com>
2020-03-29 18:30:36 +0200

    la till mer på bilder klassen

--- Template/Template/sprites/bilder.cs
+++ Template/Template/sprites/bilder.cs
@@ -41,7 +41,38 @@ namespace Template.sprites
             spriteBatch.Draw(texture, position, Color.BlueViolet);
         }
 
+        #region krocka
+        protected bool vänster(bilder sprites)
+        {
+            return this.rec.Right + this.hastighet.X < bilder.Rectangle.Left &&
+                this.rec.Left < bilder.Rectangle.Left &&
+                this.rec.Bottom < bilder.Rectangle.Top &&
+                this.rec.Top < bilder.Rectangle.Bottom &&;
+        }
 
-       
+        protected bool höger(bilder sprites)
+        {
+            return this.rec.Left + this.hastighet.X < bilder.Rectangle.Right &&
+                this.rec.Top < bilder.Rectangle.Top &&
+                this.rec.Right < bilder.Rectangle.Left &&
+                this.rec.Left < bilder.Rectangle.Right &&;
+
+        }
+        protected bool toppen(bilder sprites)
+        {
+            return this.rec.Bottom + this.hastighet.Y < texture.Rectangle.Top &&
+                this.rec.Top < texture.Rectangle.Top &&
+                this.rec.Right < texture.Rectangle.Left &&
+                this.rec.Left < texture.Rectangle.Right &&;
+        }
+        protected bool botten(bilder sprites)
+        {
+            return this.rec.Top + this.hastighet.Y < bilder.Rectangle.Bottom &&
+                this.rec.Bottom < texture.Rectangle.Bottom &&
+                this.rec.Right < texture.Rectangle.Left &&
+                this.rec.Left < texture.Rectangle.Right &&;
+        }
+        #endregion
+
     }
 }
